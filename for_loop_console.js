addf3310d16509c5451fb4f28f438b1622d8da62
MarioSeattle <marioseattle@live.com>
2017-08-13 18:48:47 -0700

    For loop from Lynda.com

---
+++ for_loop_console.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Mgomez on 8/13/17.
+ */
+var a = 5;
+var b = 5;
+var theNumbersMatch
+
+if ( a == b ) {
+    theNumbersMatch = true;
+} else {
+    theNumbersMatch = false;
+}
+
+//console.log(theNumbersMatch);
+console.log("The numbers match is: " + theNumbersMatch);
+
